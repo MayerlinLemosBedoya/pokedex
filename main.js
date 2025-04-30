b0669496c912f1b5496114bd9220dc4cb6f444c1
MayerlinLemosBedoya <maleiquianchico@hotmail.com>
2025-04-30 11:58:59 -0500

    Vista especifica

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const pokemonList= document.getElementById("pokemonList")
+const pokemonDetail=document.getElementById("pokemonDetail")
+const backtoPokedexBtn=document.getElementById("backToPokedexBtn")
+const pokemonInfo = document.getElementById("pokemonInfo")
 
 async function fetchPokemonData(pokemonId){
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
      //"https://pokeapi.co/api/v2/pokemon/"+pokemonId
      const data=  await response.json()
-     console.log(data)
      return data
     } catch (error) {
        console.error(error)
@@ -15,20 +17,45 @@ async function fetchPokemonData(pokemonId){
 function displayPokemon(pokemon){
    const pokemonCard=document.createElement("div")
     pokemonCard.classList.add("pokemon-card")
-    console.log(pokemon.sprites.front)
+   // console.log(pokemon.sprites.front)
     pokemonCard.innerHTML=`
    <h2>${pokemon.id}</h2>
     <img src="${pokemon.sprites.front_shiny}" alt="imagen de${pokemon.name}">
-    <h3>${pokemon.name}</h3>
+    <h3>${pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}</h3>
 
 
 `
+pokemonCard.addEventListener("click",()=>showPokemonDetail(pokemon))
   pokemonList.appendChild(pokemonCard)
 }
+function showPokemonDetail(pokemon){
+    pokemonList.style.display="none"
+    pokemonDetail.style.display="flex"
+    //-----codigo para obtener los stats--------
+    console.log(pokemon.stats)
+    let stats =""
+      for(let i=0;i<pokemon.stats.length;i++){
+      stats=stats + pokemon.stats[i].stat.name +"<br>"
+      }
+    //--------codigo para obtener los tipos-------
+    let types=" "
+    for(let i=0;i<pokemon.types.length;i++){
+        types=types +pokemon.types[i].type.name + "<br>"
+    }
+    pokemonInfo.classList.add(`${pokemon.types[0].type.name}`)
+    pokemonInfo.innerHTML=`
+        <h2>${pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}</h2>
+    <p>${types}</p>
+    <p>${stats}</p>
+    `
+}
+backtoPokedexBtn.addEventListener("click",()=>{
+ pokemonList.style.display="grid"
+ pokemonDetail.style.display="none"
+})
 async function loadpokdex(){
  for(let i=1;i<=52;i++){  
     const pokemon= await fetchPokemonData(i)
-    console.log(pokemon)
     displayPokemon(pokemon)
 }
 }
